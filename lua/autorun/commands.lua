a901e9de315646d1c37b44ee5c8454327704f818
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2023-02-27 09:11:11 +0100

    umm

--- lua/autorun/commands.lua
+++ lua/autorun/commands.lua
@@ -1,36 +1,36 @@
 AddCSLuaFile()
 
 concommand.Add("getbounds", function()
-	PrintTable(S37K_mapbounds[1])
+	PrintTable(S37K_mapbounds)
 end)
 
 concommand.Add("getnegativebounds", function()
-	print("negativeX = " .. S37K_mapbounds[1].negativeX ..
-		  "	negativeY = " .. S37K_mapbounds[1].negativeY ..
-		  "	negative SkyZ = " .. -S37K_mapbounds[1].skyZ)
+	print("negativeX = " .. S37K_mapbounds.negativeX ..
+		  "	negativeY = " .. S37K_mapbounds.negativeY ..
+		  "	negative SkyZ = " .. -S37K_mapbounds.skyZ)
 end)
 
 concommand.Add("getpositivebounds", function()
-	print("positiveX = " .. S37K_mapbounds[1].positiveX ..
-		  "	positiveY = " .. S37K_mapbounds[1].positiveY ..
-		  " positive SkyZ = " .. S37K_mapbounds[1].skyZ )
+	print("positiveX = " .. S37K_mapbounds.positiveX ..
+		  "	positiveY = " .. S37K_mapbounds.positiveY ..
+		  " positive SkyZ = " .. S37K_mapbounds.skyZ )
 end)
 
 concommand.Add("getskyZbounds", function()
-	print("positive SkyZ = " .. S37K_mapbounds[1].skyZ ..
-		  " negative SkyZ = " .. -S37K_mapbounds[1].skyZ	)
+	print("positive SkyZ = " .. S37K_mapbounds.skyZ ..
+		  " negative SkyZ = " .. -S37K_mapbounds.skyZ	)
 end)
 
 concommand.Add("getYbounds", function()
-	print("positiveY = " .. S37K_mapbounds[1].positiveY ..
-		  " negativeY = " .. S37K_mapbounds[1].negativeY	)
+	print("positiveY = " .. S37K_mapbounds.positiveY ..
+		  " negativeY = " .. S37K_mapbounds.negativeY	)
 end)
 
 concommand.Add("getXbounds", function()
-	print("positiveX = " .. S37K_mapbounds[1].positiveX ..
-		  "negativeX = " .. S37K_mapbounds[1].negativeX	)
+	print("positiveX = " .. S37K_mapbounds.positiveX ..
+		  "negativeX = " .. S37K_mapbounds.negativeX	)
 end)
 
 concommand.Add("getareabounds", function()
-	print("area = " .. S37K_mapbounds[1].area)
+	print("area = " .. S37K_mapbounds.area)
 end)
